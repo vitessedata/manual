5213c78090152377a8cb7da8a5c69bcbc11e0320
Feng Tian <fengttt@gmail.com>
2017-08-10 23:22:11 -0700

    Add a change.sh, change deepgreen to crystal.

---
+++ admin.trs/change.sh
@@ -0,0 +1,6 @@
+sed -i 's/deepgreen/TRS Crystal/Ig' ./install/install.lyx
+sed -i 's/deepgreen/TRS Crystal/Ig' ./perf/perf.lyx
+sed -i 's/deepgreen/TRS Crystal/Ig' ./admin.lyx
+sed -i 's/deepgreen/TRS Crystal/Ig' ./db/db.lyx
+sed -i 's/deepgreen/TRS Crystal/Ig' ./access/access.lyx
+sed -i 's/deepgreen/TRS Crystal/Ig' ./manage/manage.lyx
